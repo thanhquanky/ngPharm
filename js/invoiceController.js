fcf06c10c3593da8286cd82237cf8613a577827d
Duc <trananhduc1004@gmail.com>
2015-03-30 02:35:15 -0400

    Populate data

--- js/invoiceController.js
+++ js/invoiceController.js
@@ -1,9 +1,10 @@
 ngPharm.controller('InvoiceController',
     ['$scope', function($scope) {
-         $scope.myData = [{name: "Moroni", age: 50},
-                     {name: "Tiancum", age: 43},
-                     {name: "Jacob", age: 27},
-                     {name: "Nephi", age: 29},
-                     {name: "Enos", age: 34}];
+         $scope.myData = [{name: "Hydrocodone", unit: "bottle", quantity: 1, expiration_date: "2015/10/04", Price: 1.65},
+                          {name: "Generic Zocor ", unit: "bottle", quantity: 3, expiration_date: "2015/09/04", Price: 2.65},
+                          {name: "Lisinopril", unit: "pill", quantity: 5, expiration_date: "2015/08/04", Price: 3.65},
+                          {name: "Generic Synthroid", unit: "pill", quantity: 7, expiration_date: "2015/07/04", Price: 4.65},
+                          {name: "Generic Norvasc", unit: "bottle", quantity: 9, expiration_date: "2015/06/04", Price: 5.65},
+                          {name: "Amoxicillin ", unit: "bottle", quantity: 13, expiration_date: "2015/05/04", Price: 6.65}];
          $scope.gridOptions = { data: $scope.myData };
     }]);
