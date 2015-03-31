49f8b5144dfadf6816e21fcd26a682dc405bf60c
Thanh Ky Quan <thanhquanky@gatech.edu>
2015-03-31 01:14:22 -0400

    Fix naming, refactoring

--- js/routing.js
+++ js/routing.js
@@ -11,53 +11,13 @@ ngPharm.config(function($stateProvider, $urlRouterProvider, $resourceProvider) {
         })
         .state('invoice', {
             url: "/invoice",
-            templateUrl: "partials/invoice.html",
-            controller: "InvoiceController"
+            templateUrl: "partials/invoices.html",
+            controller: "InvoicesController"
         })
         .state('invoice.item', {
             url: "/:invoice_id",
-            templateUrl: "partials/invoice_item.html",
-            controller: function($scope, $stateParams) {
-                var invoiceName = $stateParams.invoice_id;
-                var invoice =  null;
-                angular.forEach($scope.invoices, function(currentInvoice, key) {
-                    console.log(currentInvoice);
-                    if (currentInvoice.name === invoiceName) {
-                        invoice = currentInvoice;
-                    }
-                });
-                $scope.invoice = invoice;
-
-                $scope.gridOptions = {
-                    showGridFooter: true,
-                    enableFiltering: true,
-                    columnDefs: [{
-                        name: 'Name',
-                        enableCellEdit: true,
-                        enableCellEditOnFocus: true,
-                        field: 'name',
-                        width: '13%'
-                    }, {
-                        name: "Unit",
-                        field: 'unit',
-                        width: '13%'
-                    }, {
-                        name: "Quantity",
-                        field: 'quantity',
-                        width: '13%'
-                    }, {
-                        name: "Expiration Date",
-                        field: 'expiration_date',
-                        width: '13%',
-                        cellFilter: 'date'
-                    }, {
-                        name: 'Price',
-                        field: 'price',
-                        width: '20%'
-                    }],
-                    data: 'invoice.items'
-                };
-            }
+            templateUrl: "partials/invoice.html",
+            controller: "InvoiceController"
         })
         .state('sales', {
           url: "/sales",
