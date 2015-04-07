5b99a21c94e8f92e69aa222e64c0e993c4271dbf
Thanh Ky Quan <thanhquanky@gatech.edu>
2015-04-07 02:10:11 -0400

    Add gulp for automated building

--- server.js
+++ server.js
@@ -3,11 +3,45 @@ var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var Sequelize = require('sequelize');
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.listen(3000);
 
+
+var sequelize = new Sequelize('ngPharm', 'root', 'hackathon', {
+    host: '52.5.44.90',
+    dialect: 'mariadb',
+
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    }
+});
+var User = sequelize.define('user', {
+    firstName: {
+        type: Sequelize.STRING
+    },
+    lastName: {
+        type: Sequelize.STRING
+    }
+}, {
+    freezeTableName: true // Model tableName will be the same as the model name
+});
+
+User.sync({
+    force: true
+}).then(function () {
+    // Table created
+    return User.create({
+        firstName: 'John',
+        lastName: 'Hancock'
+    });
+});
 console.log('Server started: http://localhost:3000/');
