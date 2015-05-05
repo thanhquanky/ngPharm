df55b75d689d19da1f3b2bfcd0a7259c31b28f63
Duc <trananhduc1004@gmail.com>
2015-05-05 02:24:30 -0400

    Trying to add carousel

--- public/js/controllers/homeController.js
+++ public/js/controllers/homeController.js
@@ -2,3 +2,36 @@ ngPharm.controller('HomeController',
     ['$scope', function($scope) {
 
     }]);
+
+
+ngPharm.controller('CarouselCtrl', function($scope){
+  $scope.myInterval = 1000;
+  $scope.slides = [{
+    image: '../../images/claritin.jpg',
+    text: 'This is claritin, an Anti-histanmine'
+  }, {
+    image: '../../images/amoxicillin.jpg',
+    text: 'This is amoxicillin, an Anti-biotic'
+  }, {
+    image: '../../images/doracin.jpg',
+    text: 'This is claritin, an Anti-biotic'
+  }];
+});
+
+/*
+ngPharm.controller('CarouselCtrl', function ($scope) {
+  $scope.myInterval = 5000;
+  var slides = $scope.slides = [];
+  $scope.addSlide = function() {
+    var newWidth = 600 + slides.length + 1;
+    slides.push({
+      image: 'http://placekitten.com/' + newWidth + '/300',
+      text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+    });
+  };
+  for (var i=0; i<4; i++) {
+    $scope.addSlide();
+  }
+})
+*/
