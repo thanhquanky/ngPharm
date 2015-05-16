6673759906a03ad4a788a40677fcf9f2c97147eb
Thanh Ky Quan <thanhquanky@gatech.edu>
2015-05-15 21:24:17 -0400

    Remove database from git, allow CORS for server

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ app.use(bodyParser.urlencoded({
 
 app.listen(3000);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 var drugRouter = require('./routes/drug');
