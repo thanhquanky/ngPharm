863c789fc6d905c0fd229d7ab1e550f7741335e6
Duc <trananhduc1004@gmail.com>
2015-05-19 11:04:39 -0400

    Decorating about page

--- public/js/controllers/aboutController.js
+++ public/js/controllers/aboutController.js
@@ -3,21 +3,26 @@
 
 (function() {
 
-    var ngPharm = angular.module('ngPharm');
+    var ngPharm = angular.module('ngPharm'); 
     var AboutController = function() {
         var vm = this;
         vm.developers = [
             {
                 name: "Thanh Ky Quan",
                 email: "thanhquanky@gatech.edu",
-                linkedin: "https://linkedin.com/in/thanhquanky"
+                linkedin: "https://linkedin.com/in/thanhquanky",
+                image: "thanh.png",
+                intro: "GT Class of 2015"
             },
             {
                 name: "Duc Tran",
                 email: "trananhduc1004@gmail.com",
-                linkedin: "https://www.linkedin.com/in/trananhduc1004"
+                linkedin: "https://www.linkedin.com/in/trananhduc1004",
+                image: "duc.png",
+                intro: "GT class of 2017"
             }
         ];
     }
     ngPharm.controller('AboutController', AboutController);
+
 })();
