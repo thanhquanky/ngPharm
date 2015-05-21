acb608eec567d8349d2cf265cc4f74a4b954659e
Thanh Ky Quan <thanhquanky@gatech.edu>
2015-05-21 17:17:52 -0400

    create vendor while creating new invoice

--- public/js/services/invoices.js
+++ public/js/services/invoices.js
@@ -7,17 +7,30 @@
 	angular.module('ngPharm.services')
 		.service('Invoices', ['$resource', 'InvoiceItems', function($resource, InvoiceItems){
 			var that = this;
-			that.transformDate = function(item) {
+			that.fullDateStringToDate = function(item) {
 				item.manufactureDate = moment(item.manufactureDate, "YYYY-MM-DDThh:mm:ss.sTZD").toDate();
 				item.expirationDate = moment(item.expirationDate, "YYYY-MM-DDThh:mm:ss.sTZD").toDate();
 				return item;
 			}
+			that.monthToDate = function(item) {
+				item.expirationDate = moment(item.expirationDate, 'MM/YYYY');
+				item.manufactureDate = moment(item.manufactureDate, 'MM/YYYY');
+				return item;
+			}
 			return $resource('/invoice/:invoiceId', {}, {
 				'get': {
-					transformResponse: function(data, headers) {
-						data = angular.fromJson(data);
-						data.InvoiceItems = data.InvoiceItems.map(that.transformDate);
+					transformResponse: function(data) {
+						data = angular.fromJson(data);	
+						data.InvoiceItems = data.InvoiceItems.map(that.fullDateStringToDate);
 						return data;
+					}
+				},
+				'save': {
+					method: 'POST',
+					transformRequest: function(data) {
+						data = angular.fromJson(data);
+						data.InvoiceItems = data.InvoiceItems.map(that.monthToDate);
+						return angular.toJson(data);
 					}
 				}
 			});
