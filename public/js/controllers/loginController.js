ba2887aa955a8067356fed922ebc43401137126f
Duc <trananhduc1004@gmail.com>
2015-07-12 22:55:47 -0400

    Implement logout

--- public/js/controllers/loginController.js
+++ public/js/controllers/loginController.js
@@ -1,7 +1,13 @@
-ngPharm.controller('LoginController', ['$scope', '$location', 'Auth',
-	function($scope, $location, Auth){
+ngPharm.controller('LoginController', ['$scope', '$location', '$rootScope', '$q', '$window', 'Auth',
+	function($scope, $location, $rootScope, $q, $window, Auth){
 		var vm = this;
+		vm.abc = true;
 		vm.loginData = {};
+		vm.loggedIn = Auth.isLoggedIn(); // Check if there is a token given
+		$rootScope.$on('$routeChangeStart', function() {
+			vm.loggedIn = Auth.isLoggedIn();
+		}); // Function to watch for change in loggedIn
+		//
 		vm.login = function(){
 			console.log ('Username: ' + vm.loginData.username + ". Password " + vm.loginData.password);
 			vm.processing = true; // just for the spinning icon when the server is processing.
@@ -10,65 +16,18 @@ ngPharm.controller('LoginController', ['$scope', '$location', 'Auth',
 				.success(function(data){
 					vm.processing = false; // End the process
 					// Either redirect or send message error
-					if (data.success) $location.path('/');
+					if (data.success) {
+						// $location.path('/');
+						location.reload();
+					}
 					else vm.errorMessage = data.message;
+					// console.log(vm.loggedIn);
 				}
 			);
 		};
+		//
+		vm.logout = function(){
+			Auth.logout();
+		};
 	}]
 );
-
-
-
-
-
-// angular.module('mainCtrl', [])
-// 	.controller('mainController', function($window, $scope, $rootScope, $location, Auth){
-// 		var vm = this;
-// 		// console.log('mainCtrl is alive');
-// 		vm.currentUserId = sessionStorage.getItem('userId');
-// 		console.log('new assignment at top: ' + vm.currentUserId);
-// 		$rootScope.$watch(function() {
-// 			return sessionStorage.getItem('userId');
-// 		}, function(newValue) {
-//       		vm.currentUserId = newValue;
-// 		});
-
-// 		vm.loggedIn = Auth.isLoggedIn();
-// 		$rootScope.$on('$routeChangeStart', function() {
-// 			vm.loggedIn = Auth.isLoggedIn();
-// 				// get user information on route change
-// 			Auth.getUser()
-// 			.then(function(data) {
-// 				vm.user = data;
-// 				// console.log(data);
-// 			});
-// 		});
-// 		vm.doLogin = function() {
-// 			vm.processing = true;
-// 			vm.error = '';
-// 			Auth
-// 			.login(vm.loginData.username, vm.loginData.password)
-// 			.success(function(data) {
-// 				vm.processing = false;
-// 				sessionStorage.setItem('userId', Auth.currentUserId);
-// 				vm.currentUserId = sessionStorage.getItem('userId');
-// 				console.log('currentUserId: ' + vm.currentUserId);
-// 				// if a user successfully logs in, redirect to users page
-// 				if (data.success)
-// 					$location.path('/users/' + vm.currentUserId);
-// 				else
-// 					vm.error = data.message;
-// 			});
-// 		};
-// 		vm.doLogout = function() {
-// 			Auth.logout();
-// 			sessionStorage.setItem('userId', null)
-// 			vm.currentUserId = sessionStorage.getItem('userId');
-// 			console.log("logout: " + vm.currentUserId);
-// 			// reset all user info
-// 			vm.user = {};
-// 			$location.path('/login');
-// 		};
-
-// 	});
